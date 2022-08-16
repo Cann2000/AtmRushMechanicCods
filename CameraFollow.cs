4d59b60dd29eb8f8c76a29199f8af28e5654b927
Muhammet Can Yıldız <94134588+Cann2000@users.noreply.github.com>
2022-08-16 05:19:45 +0300

    Mechanic

---
+++ CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed;
+    public Vector3 offset;
+
+    public Transform MainCharacter;
+
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, MainCharacter.position, FollowSpeed) + offset;
+    }
+}
