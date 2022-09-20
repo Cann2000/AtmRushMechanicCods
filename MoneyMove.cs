6a52171734d84cfda23a640b76a056d2e8df0cbb
Muhammet Can Yıldız <94134588+Cann2000@users.noreply.github.com>
2022-09-20 19:59:59 +0300

    GameCod

---
+++ MoneyMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyMove : MonoBehaviour
+{
+
+    // !!! PARALARIN GECIKMELI HAREKETI VE SPAWNI IÇIN !!!
+
+    Options chropt;
+
+    public Transform node;
+
+    public float nodespeed = 10;
+
+    void Update()
+    {
+        
+        chropt = GameObject.FindGameObjectWithTag("character").GetComponent<Options>();
+
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, node.position.x, Time.deltaTime * nodespeed), 5, transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("money"))
+        {
+            Destroy(other.gameObject);
+            chropt.MoneyCreate(transform);
+            
+        }
+        if (other.transform.CompareTag("Tuzak"))
+        {
+            chropt.destroyMoney();
+        }
+    }
+
+}
---
+++ SpawnPointFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointFollow : MonoBehaviour
+{
+    public float FollowSpeed;
+    public Vector3 offset;
+
+    [SerializeField] Transform MainCharacter;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, MainCharacter.position, FollowSpeed) + offset;
+    }
+}
